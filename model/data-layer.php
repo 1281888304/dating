07b8c3f44cd4ef16e6d027bfbfb6c3754969bd3c
Qinghang Zhang <Qzhang9@mail.greenriver.edu>
2020-05-12 03:01:55 -0400

    data layer

---
+++ model/data-layer.php
@@ -0,0 +1,18 @@
+<?php
+function getGender()
+{
+    $gender=array("Male","Female");
+    return $gender;
+}
+
+function getIndoor(){
+    $indoor=array("tv","movies","cooking",
+        "board game", "puzzles","readying","playing card","video game");
+    return $indoor;
+}
+
+function getoutdoor(){
+    $outdoor=array("hiking","biking", "football","running","jogging",
+        "walking","swimming","Jumping");
+    return $outdoor;
+}
