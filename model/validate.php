07847d4d4dd0c229a54e9ac559ed104b0181b871
Qinghang Zhang <Qzhang9@mail.greenriver.edu>
2020-06-04 00:55:14 -0400

    use the class for validater

--- model/validate.php
+++ model/validate.php
@@ -1,49 +1,87 @@
 <?php
-function validName($name)
+
+/**
+ * Class Validate
+ * for validation each data
+ */
+class Validate
 {
-    $name = str_replace(' ', '', $name);
-    return !empty($name) && ctype_alpha($name);
-}
-function validPhone($age){
-    if(is_numeric($age)){
-        return true;
+    /**
+     * @param $name
+     * @return bool
+     */
+    function validName($name)
+    {
+        $name = str_replace(' ', '', $name);
+        return !empty($name) && ctype_alpha($name);
     }
-    else{
-        return false;
+
+    /**
+     * @param $age
+     * @return bool if phone is a number
+     */
+    function validPhone($age)
+    {
+        if (is_numeric($age)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
-function validAge($age){
-    if ($age>18 && $age<118){
-        return true;
+
+    /**
+     * @param $age
+     * @return bool is age is large than 18 and less than 118
+     */
+    function validAge($age)
+    {
+        if ($age > 18 && $age < 118) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    else{
-        return false;
+
+    /**
+     * @param $email
+     * @return bool emailmust be corrert format
+     */
+    function validEmail($email)
+    {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
-function validEmail($email){
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        return true;
-    }else{
-        return false;
+
+    /**
+     * @param $interest
+     * @return bool value must ina rray
+     */
+    function validInterest($interest)
+    {
+        $indoor = array("tv", "movies", "cooking",
+            "boardgame", "puzzles", "readying", "playingCard", "videoGame");
+        if (in_array($interest, $indoor)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
-function validInterest($interest){
-    $indoor=array("tv","movies","cooking",
-        "boardgame", "puzzles","readying","playingCard","videoGame");
-    if(in_array($interest,$indoor)){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-function validInterest2($interest){
-    $outdoor=array("hiking","biking", "football","running","jogging",
-        "walking","swimming","Jumping");
-    if(in_array($interest,$outdoor)){
-        return true;
-    }
-    else{
-        return false;
+
+    /**
+     * @param $interest
+     * @return bool value must in array
+     */
+    function validInterest2($interest)
+    {
+        $outdoor = array("hiking", "biking", "football", "running", "jogging",
+            "walking", "swimming", "Jumping");
+        if (in_array($interest, $outdoor)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
